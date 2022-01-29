9422edf4e5a994d18d24f05d1e942fc0c5a58838
XML <547816252@qq.com>
2022-01-29 10:04:54 +0800

    2-6 修改启动引导类的位置，配置扫描的文件夹

--- src/main/java/com/jsonxia/wiki/config/WikiApplication.java
+++ src/main/java/com/jsonxia/wiki/config/WikiApplication.java
@@ -1,8 +1,11 @@
-package com.jsonxia.wiki;
+package com.jsonxia.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan({"com.jsonxia"})
 @SpringBootApplication
 public class WikiApplication {
 
