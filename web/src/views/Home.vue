c0752a25562a08f5555f8a48701cb07badf34055
XML <547816252@qq.com>
2022-01-30 15:20:18 +0800

    4-3 集成Ant Design Vue,加入示例

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+      <a-row>
+        <a-col :span="12">
+          <a-statistic title="Active Users" :value="112893" style="margin-right: 50px" />
+        </a-col>
+        <a-col :span="12">
+          <a-statistic title="Account Balance (CNY)" :precision="2" :value="112893" />
+        </a-col>
+      </a-row>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
